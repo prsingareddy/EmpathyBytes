3c2ea1294d1a6e8731b72a578241d0cf2b83267e
prsingareddy <prsingareddy6@gmail.com>
2020-10-03 19:03:02 -0400

    folder for ui

---
+++ EmpathyBytes/test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Pranathi Singareddy on 10/3/20.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
