ca0e331d31d0e00441679fcfc82b373dd94594e2
mbrigham19 <mollyrosebrigham@gmail.com>
2021-02-18 18:15:12 -0500

    Update georgiaView.swift
    added comments

--- appEB/georgiaView.swift
+++ appEB/georgiaView.swift
@@ -5,35 +5,7 @@
 //  Created by Molly Brigham on 10/28/20.
 //
 
-/*import SwiftUI
-import AVKit
-
-struct georgiaView: View {
-    
-    var body: some View {
-        VStack {
-            Text("Ringgold, Georgia: Lara Langston")
-                .font(.custom("Georgia", size: 30))
-                .fontWeight(.black)
-            VideoPlayer(player: AVPlayer(url: URL(string: "https://share.vidyard.com/watch/qRJvDxvJkGVL7b1vJvtzQm?")!)) {
-                Text("Watermark")
-                    .font(.caption)
-                    .foregroundColor(.white)
-                    .background(Color.black.opacity(0.7))
-                    .clipShape(Capsule())
-                Spacer()
-            }
-            
-        }
-        
-    }
-}
-
-struct georgiaView_Previews: PreviewProvider {
-    static var previews: some View {
-        georgiaView()
-    }
-}*/
+//This view represents the interview page with Lara Langston
 import SwiftUI
 import AVKit
 import AVFoundation
@@ -45,6 +17,7 @@ struct georgiaView: View {
     
     var body: some View {
         VStack{
+            //video player with specificities for appearance
             PlayerView()
                 .cornerRadius(15)
                 .frame(width: nil, height: maxHeight, alignment: .center)
